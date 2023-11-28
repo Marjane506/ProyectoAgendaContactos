b16cfb1ec4e4dc6eb3a7b5e9ea6b519dc7ed3870
Marjane Oliveira <marjane_rodrigues@hotmail.com>
2023-11-28 13:49:40 +0100

    Añade la clase Contact

---
+++ app/src/main/java/net/smartmind/agenda/models/Contact.java
@@ -0,0 +1,33 @@
+package net.smartmind.agenda.models;
+
+public class Contact {
+    private String name;
+    private String phone;
+
+    public Contact(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Contact setName(String name) {
+        this.name = name;
+         return this;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public Contact setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+    
+
+    
+}
