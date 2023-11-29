6ce8478c0320b53f817dc13061e9c89b4be8a2d4
Marjane Oliveira <marjane_rodrigues@hotmail.com>
2023-11-29 13:31:20 +0100

    Añandio Cambios en la class Contact metodo to String

--- app/src/main/java/net/smartmind/agenda/models/Contact.java
+++ app/src/main/java/net/smartmind/agenda/models/Contact.java
@@ -3,7 +3,6 @@ package net.smartmind.agenda.models;
 public class Contact {
     private String name;
     private String phone;
-
     public Contact(String name, String phone) {
         this.name = name;
         this.phone = phone;
@@ -28,6 +27,11 @@ public class Contact {
         return this;
     }
     
+
+    @Override
+    public String toString() {
+        return "Contact [name=" + name + ", phone=" + phone + "]";
+    }
 
     
 }
